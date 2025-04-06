1823d21928522215d8060ecd4f29e8b1ab2b453b
Utkarsh Patel <115659773+utkarshpat@users.noreply.github.com>
2025-04-07 02:06:13 +0530

    Update SmartHid.py

--- SmartHid.py
+++ SmartHid.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 import firebase_admin
 from firebase_admin import credentials, db
-
+import json
 # Firebase Initialization
 if not firebase_admin._apps:
     try:
